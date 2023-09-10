a56a99830043bb9cbf04878c1b89e68398de4c95
kira-osm <bousserbat@gmail.com>
2023-09-11 01:18:08 +0200

    Update & Delete actor
    Update & Delete actor

--- Actors/Controllers/ActorControllerWriter.js
+++ Actors/Controllers/ActorControllerWriter.js
@@ -14,25 +14,59 @@ class ActorControllerWriter {
   }
 
   static async updateActor(req, res) {
-    const { name, age } = req.body;
-    const { id } = req.params;
     try {
-      const actor = await Actor.findByIdAndUpdate(id, { name, age }, { new: true });
-      res.json(actor);
+       // req.query pour extraire les paramètres de l'URL
+      const { firstName, lastName } = req.query;
+      
+      //req.body pour extraire les données de mise à jour
+      const updatedData = req.body; 
+      
+      const query = {
+        $or: [
+          { firstName },
+          { lastName },
+        ],
+      };
+  
+      const updatedActor = await Actor.findOneAndUpdate(query, updatedData, { new: true });
+  
+      if (!updatedActor) {
+        return res.status(404).json({ error: 'Actor not found' });
+      }
+  
+      res.json({ message: 'Actor updated successfully', actor: updatedActor });
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Error updating actor' });
     }
-  }
+}
 
   static async deleteActor(req, res) {
-    const { id } = req.params;
     try {
-      await Actor.findByIdAndRemove(id);
-      res.json({ message: 'Actor deleted successfully' });
+      const { firstName, lastName } = req.query;
+
+      if (!firstName && !lastName) {
+        return res.status(400).json({ error: 'Veuillez fournir un prénom ou un nom de famille pour la suppression.' });
+      }
+
+      let deletedActor;
+
+      if (firstName) {
+        // Supprimer par prénom
+        deletedActor = await Actor.findOneAndDelete({ firstName });
+      } else {
+        // Supprimer par nom de famille
+        deletedActor = await Actor.findOneAndDelete({ lastName });
+      }
+
+      if (!deletedActor) {
+        return res.status(404).json({ error: 'Aucun acteur trouvé pour la suppression.' });
+      }
+
+      res.json({ message: 'Acteur supprimé avec succès' });
     } catch (error) {
       console.error(error);
-      res.status(500).json({ error: 'Error deleting actor' });
+      res.status(500).json({ error: 'Erreur lors de la suppression de l\'acteur' });
     }
   }
 }
--- Actors/Routes/ActorRoutes.js
+++ Actors/Routes/ActorRoutes.js
@@ -3,13 +3,15 @@ const router = express.Router();
 const ActorControllerReader = require('../Controllers/ActorControllerReader');
 const ActorControllerWriter = require('../Controllers/ActorControllerWriter');
 
-router.get('/show-actors', ActorControllerReader.getAllActors);
-router.get('/search-actor', ActorControllerReader.searchByFirstNameOrLastNameOrBoth);
 
 
 
-router.post('/add-actor', ActorControllerWriter.createActor);
-router.put('/actors/:id', ActorControllerWriter.updateActor);
-router.delete('/actors/:id', ActorControllerWriter.deleteActor);
+router.post('/actor/add-actor', ActorControllerWriter.createActor);
+router.put('/actor/update-actor', ActorControllerWriter.updateActor);
+router.delete('/actor/delete-actor', ActorControllerWriter.deleteActor);
+
+
+router.get('/actor/show-actors', ActorControllerReader.getAllActors);
+router.get('/actor/search-actor', ActorControllerReader.searchByFirstNameOrLastNameOrBoth);
 
 module.exports = router;
